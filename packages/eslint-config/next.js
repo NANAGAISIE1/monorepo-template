af0125c9e8abf536d6e494caaf6ebd7477bf9589
Nana Gaisie <nabgaisie@outlook.com>
2024-07-29 00:57:23 +0000

    chore: Add initial project files and configurations

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,35 +1,67 @@
-const { resolve } = require("node:path");
+const { resolve } = require("node:path")
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), "tsconfig.json")
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "prettier",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    "turbo",
-  ],
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  env: {
-    node: true,
-    browser: true,
-  },
-  plugins: ["only-warn"],
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
+    extends: [
+        "eslint:recommended",
+        "prettier",
+        require.resolve("@vercel/style-guide/eslint/next"),
+        "turbo",
+        "plugin:import/recommended",
+        "plugin:prettier/recommended",
+    ],
+    globals: {
+        React: true,
+        JSX: true,
     },
-  },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-  ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
-};
+    env: {
+        node: true,
+        browser: true,
+    },
+    plugins: ["only-warn"],
+    settings: {
+        "import/resolver": {
+            typescript: {
+                project,
+            },
+        },
+    },
+    ignorePatterns: [
+        // Ignore dotfiles
+        ".*.js",
+        "node_modules/",
+    ],
+    overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
+    rules: {
+        "import/order": [
+            "warn",
+            {
+                alphabetize: {
+                    caseInsensitive: true,
+                    order: "asc",
+                },
+                groups: [
+                    "builtin",
+                    "external",
+                    "internal",
+                    ["parent", "sibling"],
+                    "index",
+                ],
+                "newlines-between": "always",
+                pathGroups: [
+                    {
+                        pattern: "@/**",
+                        group: "internal",
+                    },
+                    {
+                        pattern: "~/**",
+                        group: "internal",
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ["builtin"],
+            },
+        ],
+    },
+}
